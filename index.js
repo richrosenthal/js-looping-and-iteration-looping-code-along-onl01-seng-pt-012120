dd1b4f72723f6995e5a2e978b2179e92210df097
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-07-28 23:58:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function wrapGifts(gifts) {
 }
  
 wrapGifts(gifts);
+
+const names = ["Ada", "Brendan", "Ali"];
+const event = "birthday";
+
+function writeCards(names, event){
+  for (let i = 0; i < names.length; i++){
+    console.log(`Thank you, ${names[i]} for the wonderful ${event} gift!`);
+  }
+  return names 
+}
