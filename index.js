e773fa6964d83f296030faa91aa2ee07f921fa89
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-07-29 00:11:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+const names = [];
+const event = "birthday";
+const messages = [];
+const number = 10; 
+
+function writeCards(names, event){
+  for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages 
+}
+
+writeCards(names, event);
+
+function countDown(number){
+  while (number > -1){
+    console.log(number)
+    number = number - 1 
+  }
+}
